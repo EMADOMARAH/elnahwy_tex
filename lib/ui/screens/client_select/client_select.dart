d595eb368fdaf714eb14edde7ce43cfa276d33ed
Dabash99 <61636920+Dabash99@users.noreply.github.com>
2021-02-18 23:09:22 +0200

    Client Select Search

--- lib/ui/screens/client_select/client_select.dart
+++ lib/ui/screens/client_select/client_select.dart
@@ -1,27 +1,46 @@
-import 'package:elnahwy_tex/Widget/background_color.dart';
 import 'package:flutter/material.dart';
 
 class Client_Select extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Background_color(
-      child: Column(
-        children: <Widget>[
-          Row(
-            children: <Widget>[
-              Text("A7a"),
-              Container(
-                height: 48,
-                width: 236,
-                decoration: BoxDecoration(
-                  color: Colors.red,
-                  borderRadius: BorderRadius.circular(10),
+    return Scaffold(
+        body :SafeArea(
+          child: Container(
+            height: double.infinity,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                gradient: new LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                    Color(0xffC3FCF2),
+                    Color(0xff659B91),
+                  ],
                 ),
+              ),
+              child: Column(
+                children: <Widget>[
+                  Flexible(child: TextField(
+                      keyboardType: TextInputType.text,
+                      decoration: InputDecoration(
+                          hintText: 'Search',
+                          prefixStyle: TextStyle(color: Color(0xff078547)),
+                          hintStyle: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 15,
+                          ),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(100),
+                              borderSide: new BorderSide(color: Color(0xff078547))),
+                          contentPadding: EdgeInsets.all(10)
+                      )
+                  ),),
+                  SizedBox(width: 10,),
+                  Image(image: AssetImage("images/Search.png"))
+                ],
               )
-            ],
-          )
-        ],
-      ),
+          ),
+        )
     );
   }
 }
