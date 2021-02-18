a5c8e36c0ed0a8235b72d97d398a7d581f3c5d1a
Dabash99 <61636920+Dabash99@users.noreply.github.com>
2021-02-18 23:37:07 +0200

    Search added

--- lib/ui/screens/client_select/client_select.dart
+++ lib/ui/screens/client_select/client_select.dart
@@ -4,55 +4,69 @@ class Client_Select extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body :SafeArea(
-          child: Container(
-            height: double.infinity,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                gradient: new LinearGradient(
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                  colors: [
-                    Color(0xffC3FCF2),
-                    Color(0xff659B91),
-                  ],
-                ),
-              ),
-              child: SingleChildScrollView(
-                child: Padding(
-                  padding: EdgeInsets.all(15),
-                  child: Column(
+        body: SafeArea(
+      child: Container(
+          height: double.infinity,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            gradient: new LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Color(0xffC3FCF2),
+                Color(0xff659B91),
+              ],
+            ),
+          ),
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.all(15),
+              child: Column(
+                children: <Widget>[
+                  Row(
                     children: <Widget>[
-                      Row(
-                        children: <Widget>[
-                          Flexible(child: TextFormField(
-                              keyboardType: TextInputType.text,
-                              textAlign: TextAlign.center,
-                              decoration: InputDecoration(
-                                fillColor: Colors.white.withOpacity(0.54),
-                                  filled: true,
-                                  hintText: 'بحــث',
-                                  focusColor: Colors.white,
-                                  hintStyle: TextStyle(
-                                    color: Colors.black,
-                                    fontSize: 15,
-                                    fontFamily:"Cairo",
+                      Flexible(
+                        child: TextFormField(
+                            keyboardType: TextInputType.text,
+                            textAlign: TextAlign.center,
+                            decoration: InputDecoration(
+                                prefixIcon: Padding(
+                                  padding: EdgeInsets.only(
+                                      left: 10, top: 10, bottom: 10),
+                                  child: Image.asset(
+                                    'images/Search.png',
+                                    width: 10,
+                                    height: 10,
                                   ),
-                                  border: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(100),),
-                                  contentPadding: EdgeInsets.all(10)
-                              )
-                          ),),
-                          SizedBox(width: 20,),
-                          Image(image: AssetImage('images/Client.png'),height: 66,width: 66,)
-                        ],
+                                ),
+                                fillColor: Colors.white.withOpacity(0.54),
+                                filled: true,
+                                hintText: 'بحــث',
+                                focusColor: Colors.white,
+                                hintStyle: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 15,
+                                  fontFamily: "Cairo",
+                                ),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(100),
+                                ),
+                                contentPadding: EdgeInsets.all(10))),
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Image(
+                        image: AssetImage('images/Client.png'),
+                        height: 66,
+                        width: 66,
                       )
                     ],
-                  ),
-                ),
-              )
-          ),
-        )
-    );
+                  )
+                ],
+              ),
+            ),
+          )),
+    ));
   }
 }
