f1d05efbff0f3a6729b979a9ddf005b678ff085b
Dabash99 <61636920+Dabash99@users.noreply.github.com>
2021-02-18 21:34:49 +0200

    Class for background

---
+++ lib/ui/Widget/background_color.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+class Background_color extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: new LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [
+            Color(0xffC3FCF2),
+            Color(0xff659B91),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/ui/screens/client_select/client_select.dart
+++ lib/ui/screens/client_select/client_select.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class Client_Select extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      
+    );
   }
 }
