f6570716c30d9ecfb491041c17ad3bf45644295c
Dabash99 <61636920+Dabash99@users.noreply.github.com>
2021-02-18 21:21:31 +0200

    Client Select Created

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.indigo,
+        primarySwatch: Colors.blue,
         //visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: Home_Screen(),
