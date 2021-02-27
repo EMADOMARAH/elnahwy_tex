073dcbfb5e829bb957fbbd9a2db4e137b330be56
Dabash99 <61636920+Dabash99@users.noreply.github.com>
2021-02-27 11:21:26 +0200

    ...

--- lib/ui/screens/client_data/client_data.dart
+++ lib/ui/screens/client_data/client_data.dart
@@ -7,39 +7,28 @@ import 'package:elnahwy_tex/widget/cust_txtformfield.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:page_transition/page_transition.dart';
+
 class client_data extends StatefulWidget {
   @override
   _client_dataState createState() => _client_dataState();
 }
 
 class _client_dataState extends State<client_data> {
-  TextEditingController  clothName;
+  TextEditingController clothName;
   TextEditingController clothtype;
   TextEditingController clothtupenumber;
   TextEditingController clothNote;
+
   @override
   Widget build(BuildContext context) {
-
-
     return Scaffold(
       appBar: AppBar(
-          automaticallyImplyLeading: false,
+          automaticallyImplyLeading: true,
           backgroundColor: Color(0xff659B91),
           title: Row(
             children: <Widget>[
-              IconButton(
-                icon: Icon(
-                  Icons.arrow_back,
-                ),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    PageTransition(
-                      type: PageTransitionType.rightToLeft,
-                      child: ClientPage(),
-                    ),
-                  );
-                },
+              SizedBox(
+                width: 10,
               ),
               Spacer(
                 flex: 1,
@@ -62,22 +51,26 @@ class _client_dataState extends State<client_data> {
                 Color(0xff659B91),
               ],
             ),
-
-          )
-          , child:Container(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: <Widget>[
-              cust_label("اسم العميل"),
-              cust_txtformfield("اسم العميل", TextInputType.text),
-              Container_client_cloth(clothName,clothtupenumber,clothtype,clothNote),
-              Container_client_cloth(clothName,clothtupenumber,clothtype,clothNote),
-              Container_client_cloth(clothName,clothtupenumber,clothtype,clothNote),
-              Container_client_cloth(clothName,clothtupenumber,clothtype,clothNote),
-              Container_client_cloth(clothName,clothtupenumber,clothtype,clothNote),
-            ],
           ),
-        ),
+          child: Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: <Widget>[
+                cust_label("اسم العميل"),
+                cust_txtformfield("اسم العميل", TextInputType.text),
+                Container_client_cloth(
+                    clothName, clothtupenumber, clothtype, clothNote),
+                Container_client_cloth(
+                    clothName, clothtupenumber, clothtype, clothNote),
+                Container_client_cloth(
+                    clothName, clothtupenumber, clothtype, clothNote),
+                Container_client_cloth(
+                    clothName, clothtupenumber, clothtype, clothNote),
+                Container_client_cloth(
+                    clothName, clothtupenumber, clothtype, clothNote),
+              ],
+            ),
+          ),
         ),
       ),
     );
