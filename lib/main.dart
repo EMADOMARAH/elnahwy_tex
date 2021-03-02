0377604fd12382fe8a7285341da53b6ac7f94586
Dabash99 <61636920+Dabash99@users.noreply.github.com>
2021-03-03 01:51:04 +0200

    last commit insha'Allah :""

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.indigo,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Splash_Screen(),
+      home:  Splash_Screen(),
     );
   }
 }
